3a3124306c2855ad096852ea6200d9577779051f
Lucas Morais <lucascdemorais@gmail.com>
2022-07-23 16:29:09 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@AVL-Tree/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <set>
+#include <string>
+
+class FileNode {
+  unsigned int fileIndex;
+  unsigned int count;
+  std::set<unsigned int> lines;
+};
+
+class WordNode {
+  std::string word;
+  unsigned int count;
+  std::list<FileNode> files;
+};
+
+int main() { std::cout << "Hello World!\n"; }
